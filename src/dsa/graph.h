6005d4688969efb70818cc0d868db6fbe9b106d3
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-12-28 22:01:47 -0500

    Fix clang-tidy warnings

--- src/dsa/graph.h
+++ src/dsa/graph.h
@@ -1,6 +1,7 @@
 #ifndef GRAPH_H_
 #define GRAPH_H_
 
+#include <cstddef>
 #include <initializer_list>
 #include <vector>
 
@@ -11,7 +12,7 @@ class UndirectedGraph final {
  public:
   UndirectedGraph() = default;
 
-  explicit UndirectedGraph(std::initializer_list<T> vertices) noexcept {
+  UndirectedGraph(std::initializer_list<T> vertices) noexcept {
     for (const T &vertex : vertices) {
       vertices_.emplace_back(vertex);
     }
@@ -19,7 +20,7 @@ class UndirectedGraph final {
     const size_t size = vertices_.size();
 
     for (size_t i = 0; i < size; ++i) {
-        adj_matrix_.emplace_back(std::vector<bool>(size, false));
+      adj_matrix_.emplace_back(size, false);
     }
   }
 
@@ -33,26 +34,38 @@ class UndirectedGraph final {
     adj_matrix_[to][from] = false;
   }
 
+  [[nodiscard]]
   bool HasEdge(const size_t from, const size_t to) const noexcept {
     return adj_matrix_[from][to];
   }
 
-  void Insert(const size_t index, const T &vertex) noexcept {
+  void Insert(const ptrdiff_t index, const T &vertex) noexcept {
     const size_t new_size = Size() + 1;
 
     vertices_.insert(vertices_.begin() + index, vertex);
 
     for (std::vector<bool> &row : adj_matrix_) {
-       row.insert(row.begin() + index, false);
+      row.insert(row.begin() + index, false);
     }
 
-    adj_matrix_.insert(adj_matrix_.begin() + index, std::vector<bool>(new_size, false));
+    adj_matrix_.insert(adj_matrix_.begin() + index,
+                       std::vector<bool>(new_size, false));
   }
 
-  size_t Size() const noexcept { return vertices_.size(); }
+  [[nodiscard]]
+  size_t Size() const noexcept {
+    return vertices_.size();
+  }
 
-  const std::vector<std::vector<bool>> &adj_matrix() const noexcept { return adj_matrix_; }
-  const std::vector<T> &vertices() const noexcept { return vertices_; }
+  [[nodiscard]]
+  const std::vector<std::vector<bool>> &adj_matrix() const noexcept {
+    return adj_matrix_;
+  }
+
+  [[nodiscard]]
+  const std::vector<T> &vertices() const noexcept {
+    return vertices_;
+  }
 
  private:
   std::vector<std::vector<bool>> adj_matrix_;
